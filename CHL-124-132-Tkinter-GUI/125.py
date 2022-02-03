ebc7138f52d4d3eb451112ba9873d1be79a5e171
Nihat Halici <nihat.halici@gmail.com>
2022-02-03 09:58:34 +0100

    Update 125.py

--- CHL-124-132-Tkinter-GUI/125.py
+++ CHL-124-132-Tkinter-GUI/125.py
@@ -1,6 +1,6 @@
 '''
 Write a program that can be used instead of rolling a six-sided
-die in a board game. When the user clicks a button it should
+dice in a board game. When the user clicks a button it should
 display a random whole number between 1 to 6 (inclusive).
 '''
 from tkinter import *
