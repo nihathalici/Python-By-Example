e0c6104265376bb18f4fd547c8396dd023493203
Nihat Halici <nihat.halici@gmail.com>
2022-02-14 09:50:04 +0100

    Add files via upload

---
+++ Alternative-Solutions/CHL-001-011-The-Basics/AS-001.py
@@ -0,0 +1,15 @@
+'''
+Ask for the user’s first name and display the output message
+Hello [First Name] .
+'''
+
+#####
+# Python By Example
+# Exercise 001
+# Christopher Hagan
+#####
+
+
+firstName = input('What is your forename? ')
+
+print('Hello {}'.format(firstName))
