be7303eb75531b2afd0d8faecb39f8ce2090a2fc
Nihat Halici <nihat.halici@gmail.com>
2022-03-01 08:52:52 +0100

    Add files via upload

---
+++ Alternative-Solutions/CHL-052-059-Random/AS-052.py
@@ -0,0 +1,14 @@
+'''
+AS-052
+Display a random integer between 1 and 100 inclusive.
+'''
+
+#####
+# Python By Example
+# Exercise 052
+# Christopher Hagan
+#####
+
+import random
+
+print('Random integer value is: {}'.format(random.randint(1, 100)))
