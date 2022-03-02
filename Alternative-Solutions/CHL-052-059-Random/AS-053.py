9d471eb236055cfd36c6d4ce5b6f461bd6f76af1
Nihat Halici <nihat.halici@gmail.com>
2022-03-02 09:05:35 +0100

    Add files via upload

---
+++ Alternative-Solutions/CHL-052-059-Random/AS-053.py
@@ -0,0 +1,14 @@
+'''
+AS-053
+Display a random fruit from a list of five fruits.
+'''
+
+#####
+# Python By Example
+# Exercise 053
+# Christopher Hagan
+#####
+
+import random
+
+print('A random fruit choice would be: {}'.format(random.choice(['Apple', 'Pear', 'Grape', 'BlueBerry', 'Pomegranate'])))
