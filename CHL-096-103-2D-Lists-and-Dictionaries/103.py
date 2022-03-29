b573bf1b92a4904f18eb2b1394fcfa439219701c
Nihat Halici <nihat.halici@gmail.com>
2022-03-29 07:58:27 +0200

    Update 103.py

--- CHL-096-103-2D-Lists-and-Dictionaries/103.py
+++ CHL-096-103-2D-Lists-and-Dictionaries/103.py
@@ -1,13 +1,10 @@
 '''
-0
-04
 After gathering the four names, ages and shoe sizes, ask the
 user to enter the name of the person they want to remove from
 the list. Delete this row from the data and display the other rows
 on separate lines.
-
-
 '''
+
 list = {}
 for i in range(0, 4):
     name = input("Enter name: ")
@@ -18,5 +15,3 @@ for i in range(0, 4):
 ask = input("Enter a name to see details: ")
 
 print( "{} is {} years old.".format(ask, list[ask]['Age'])  )
-
-
