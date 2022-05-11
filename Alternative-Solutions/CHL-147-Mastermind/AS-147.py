d64bdb833aa176338d5b0fef07e08bd1c14f5a16
Nihat Halici <nihat.halici@gmail.com>
2022-05-11 11:56:48 +0200

    Update AS-147.py

--- Alternative-Solutions/CHL-147-Mastermind/AS-147.py
+++ Alternative-Solutions/CHL-147-Mastermind/AS-147.py
@@ -8,12 +8,27 @@ You are going to make an on-screen version of the board game
 import random
 
 def select_col():
-    pass
+    colours = ["r", "b", "o", "y", "p", "g", "w"]
+    c1 = random.choice(colours)
+    c2 = random.choice(colours)
+    c3 = random.choice(colours)
+    c4 = random.choice(colours)
+    data = (c1, c2, c3, c4)
 
 def tryit(c1, c2, c3, c4):
     pass
 
 def main():
-    pass
+    (c1, c2, c3, c4) = select_col()
+    score = 0
+    play = True
+    while play == True:
+        (correct, wrong_place) = tryit(c1, c2, c3, c4)
+        score += 1
+        if correct  == 4:
+            play = False
+    print("You win!")
+    print("You took", score, "guesses")
+    
 
 main()
