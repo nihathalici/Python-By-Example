d3d7367e6c626459070c86c09af6c5177b7f09ba
Nihat Halici <nihat.halici@gmail.com>
2022-05-11 11:43:06 +0200

    Add files via upload

---
+++ Alternative-Solutions/CHL-147-Mastermind/AS-147.py
@@ -0,0 +1,19 @@
+"""
+AS-147
+Mastermind:
+You are going to make an on-screen version of the board game 
+“Mastermind”.
+"""
+
+import random
+
+def select_col():
+    pass
+
+def tryit(c1, c2, c3, c4):
+    pass
+
+def main():
+    pass
+
+main()
