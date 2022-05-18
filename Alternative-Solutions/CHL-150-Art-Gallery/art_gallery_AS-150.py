f81144dcad6e326d2ebbe53b56e8f62833c9930d
Nihat Halici <nihat.halici@gmail.com>
2022-05-18 14:15:24 +0200

    Add files via upload

---
+++ Alternative-Solutions/CHL-150-Art-Gallery/art_gallery_AS-150.py
@@ -0,0 +1,59 @@
+#####
+# Python By Example
+# Exercise 150
+# Christopher Hagan
+##
+#####
+
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
+
+import os, sqlite3, tkinter.messagebox, csv
+
+soldArtworkFile = 'soldArtwork.csv'
+soldArtworkColumnHeaders = ['PieceID', 'Artist', 'Title', 'Medium', 'Price']
+
+def generateListOfArtists():
+    pass
+
+def clearNewArtistFields():
+    pass
+
+def clearNewArtworkFields():
+    pass
+
+def clearArtworkTree():
+    pass
+
+def displayArtist(artistKey):
+    pass
+
+def displayArtwork(contentsOfPiecesOfArt):
+    pass
+
+def loadAllArtwork():
+    pass
+
+def clearSearchFilter():
+    pass
+
+def artworkSelected(selection):
+    pass
+
+def addArtist():
+    pass
+
+def addPieceOfArt():
+    pass
+
+def pieceOfArtSold():
+    pass
+
+def searchArtwork():
+    pass
+
+
+
+
+
