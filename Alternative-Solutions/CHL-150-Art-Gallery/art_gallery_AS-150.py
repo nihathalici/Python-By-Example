17081ca076baecebe11ec2e268a9f598b4d26176
Nihat Halici <nihat.halici@gmail.com>
2022-05-20 12:14:26 +0200

    Update art_gallery_AS-150.py

--- Alternative-Solutions/CHL-150-Art-Gallery/art_gallery_AS-150.py
+++ Alternative-Solutions/CHL-150-Art-Gallery/art_gallery_AS-150.py
@@ -15,10 +15,20 @@ soldArtworkFile = 'soldArtwork.csv'
 soldArtworkColumnHeaders = ['PieceID', 'Artist', 'Title', 'Medium', 'Price']
 
 def generateListOfArtists():
-    pass
+    artistList = []
+    artistQuery = cursor.execute("""SELECT ArtistID, Name FROM Artists;""")
+    for artist in artistQuery.fetchall():
+        artistList.append('{} - {}'.format(artist[0], artist[1]))
+    
+    artistCombobox.config(values=artistList)
 
 def clearNewArtistFields():
-    pass
+    nameEntry.delete(0, 'end')
+    addressEntry.delete(0, 'end')
+    townEntry.delete(0, 'end')
+    countyEntry.delete(0, 'end')
+    postcodeEntry.delete(0, 'end')
+
 
 def clearNewArtworkFields():
     pass
@@ -79,19 +89,111 @@ window.title('Art Gallery')
 artistFrame = LabelFrame(text='Artist')
 artistFrame.place(x=50, y=25, width=900, height=300)
 
+artistListBox = Listbox()
+artistListBox.place(x=150, y=75, width=300, height=150)
+displayArtist(0)
+
 # SUBFRAME: New Artist
 newArtistFrame = LabelFrame(text='Add new Artist')
 newArtistFrame.place(x=600, y=50, width=330, height=230)
+
+nameLabel = Label(text='Name: ')
+nameLabel.place(x=610, y=75, width=75, height=25)
+nameEntry = Entry(text='')
+nameEntry.place(x=685, y=75, width=150, height=25)
+
+addressLabel = Label(text='Address: ')
+addressLabel.place(x=610, y=100, width=75, height=25)
+addressEntry = Entry(text='')
+addressEntry.place(x=685, y=100, width=150, height=25)
+
+townLabel = Label(text='Town: ')
+townLabel.place(x=610, y=125, width=75, height=25)
+townEntry = Entry(text='')
+townEntry.place(x=685, y=125, width=150, height=25)
+
+countyLabel = Label(text='County: ')
+countyLabel.place(x=610, y=150, width=75, height=25)
+countyEntry = Entry(text='')
+countyEntry.place(x=685, y=150, width=150, height=25)
+
+postcodeLabel = Label(text='Postcode: ')
+postcodeLabel.place(x=610, y=175, width=75, height=25)
+postcodeEntry = Entry(text='')
+postcodeEntry.place(x=685, y=175, width=150, height=25)
+
+addArtistButton = Button(text='Add Artist', command=addArtist)
+addArtistButton.place(x=740, y=215, width=75, height=50)
 
 # SECOND MAIN FRAME:Artwork Frame and Contents
 artworkFrame = LabelFrame(text='Artwork')
 artworkFrame.place(x=50, y=375, width=900, height=600)
 
+artworkFrame = ttk.Treeview(show='headings', column=('Title', 'Medium', 'Price'), selectmode='browse')
+artworkFrame.column('Title', width=300, anchor='w')
+artworkFrame.heading('Title', text='Title')
+artworkFrame.column('Medium', width=150, anchor='center')
+artworkFrame.heading('Medium', text='Medium')
+artworkFrame.column('Price', width=150, anchor='e')
+artworkFrame.heading('Price', text='Price (£)')
+artworkFrame.bind('<<TreeviewSelect>>', artworkSelected)
+artworkFrame.place(x=75, y=400, width=600, height=400)
+loadAllArtwork()
+
 # SUBFRAME: FiltersFrame
-FiltersFrame = LabelFrame(text='Filter')
-FiltersFrame.place(x=750, y=400, width=150, height=225)
+filtersFrame = LabelFrame(text='Filter')
+filtersFrame.place(x=750, y=400, width=150, height=225)
+
+searchLabel = Label(text='Search criteria')
+searchLabel.place(x=775, y=425, width=100, height=25)
+
+searchCriteriaCombobox = ttk.Combobox(values=['Artist', 'Medium', 'Price'])
+searchCriteriaCombobox.place(x=775, y=450, width=100, height=25)
+
+searchForLabel = Label(text='Search for')
+searchForLabel.place(x=775, y=475, width=100, height=25)
+
+searchForEntry = Entry(text='')
+searchForEntry.place(x=775, y=500, width=100, height=25)
+
+searchButton = Button(text='Search', command=searchArtwork)
+searchButton.place(x=775, y=545, width=100, height=25)
+
+clearFilterButton = Button(text='Clear Filter', command=clearSearchFilter)
+clearFilterButton.place(x=775, y=585, width=100, height=25)
+
+sellArtworkButton = Button(text='Mark Artwork\n as sold', command=pieceOfArtSold)
+sellArtworkButton.place(x=750, y=700, width=150, height=50)
+
+# SUBFRAME: newArtworkFrame
+
+newArtworkFrame = LabelFrame(text='Add new artwork')
+newArtworkFrame.place(x=75, y=825, width=130, height=850)
+
+titleLabel = Label(text='Title -')
+titleLabel.place(x=100, y=850, width=75, height=25)
+titleEntry = Entry(text='')
+titleEntry.place(x=175, y=850, width=400, height=25)
+
+mediumLabel = Label(text='Medium -')
+mediumLabel.place(x=100, y=875, width=75, height=25)
+mediumEntry = Entry(text='')
+mediumEntry.place(x=175, y=875, width=100, height=25)
+
+priceLabel = Label(text='Price (£) -')
+priceLabel.place(x=100, y=900, width=75, height=25)
+priceEntry = Entry(text='')
+priceEntry.place(x=175, y=900, width=100, height=25)
+
+artistLabel = Label(text='Artist -')
+artistLabel.place(x=300, y=885, width=75, height=25)
+
+artistCombobox = ttk.Combobox(values='')
+artistCombobox.place(x=400, y=885, width=150, height=25)
+generateListOfArtists()
+
+addPieceOfArtButton = Button(text='Add Piece of\n  Art', command=addPieceOfArt)
+addPieceOfArtButton.place(x=700, y=870, width=100, height=50)
 
 window.mainloop()
 db.close()
-
-
